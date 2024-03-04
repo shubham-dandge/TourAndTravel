a1ef97140f14dea0da7745b8714cf45718b8626a
DANDGE Shubham (INTERN) <SDE77@dsone.3ds.com>
2024-03-04 17:06:29 +0530

    menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
 import { Component } from "react";
 import "./NavbarStyles.css";
-
+import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component  {
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Trippy</h1>
-                <ul className="nav-menu">
-                    <li>
-                        <a href="/">
-                        <i className="fa-solid fa-house-user"></i>Home
-                        </a>
-                    </li>
+                <ul className="nav-menu">{MenuItems.map((item,index) => {
+                    return (
+                        <li key={index}><a href="/"><i className={item.icon}></i>{item.title}</a></li>
+                    )
+                })}
+                    
                 </ul>
             </nav>
         )
