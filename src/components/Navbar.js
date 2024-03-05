548836c039bd8dbbe422f9cc50050cf076dec043
DANDGE Shubham (INTERN) <SDE77@dsone.3ds.com>
2024-03-05 18:09:47 +0530

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,34 @@
 import { Component } from "react";
 import "./NavbarStyles.css";
 import { MenuItems } from "./MenuItems";
-
-class Navbar extends Component  {
-    render(){
-        return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Trippy</h1>
-                <ul className="nav-menu">{MenuItems.map((item,index) => {
-                    return (
-                        <li key={index}><a href="/"><i className={item.icon}></i>{item.title}</a></li>
-                    )
-                })}
-                    
-                </ul>
-            </nav>
-        )
-    }
+// 1-1-36
+class Navbar extends Component {
+  state = {clicked: false};
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  }
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">SDE77</h1>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className="nav-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href="/">
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
+        </ul>
+      </nav>
+    );
+  }
 }
 export default Navbar;
